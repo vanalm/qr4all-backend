3b3afea3d919d4bbae39da52a6ea0bfb2ccdeb6e
Jake <jake@cocowireless.org>
2024-03-29 09:32:55 -1000

    setup for variables input

--- server.py
+++ server.py
@@ -7,29 +7,24 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS
 
 app= Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:8080"])
+# CORS(app, origins=["http://127.0.0.1:8080"])
+CORS(app, origins=["http://127.0.0.1:5500"])
 
 default_scale = 2.5
 @app.route('/generate', methods=['POST'])
 def create_qr_code():
     data = request.json
-    qr_content = data.get('text', 'HI!')  # Access the 'text' value, default to '' if not found
+    qr_content = data.get('inputString', 'www.qr4thepeople.com')  # Access the 'text' value, default to '' if not found
+    scaleValue = data.get('scaleValue', default_scale)
     light_main = data.get('light_main', 'white')  # Access the 'text' value, default to '' if not found
     dark_main = data.get('dark_main', 'black')  # Access the 'text' value, default to '' if not found
-    border_color = data.get('border_color', 'white')  # Access the 'text' value, default to '' if not found
-
-    # try:
-    #     scale=float(data.get('scale', '2')),
-    # except Exception as e:
-    #     print(f"Could not convert {data.get('scale')} to type float, defaulting to {default_scale}")
-    #     scale = default_scale
-
+    data_light=data.get('data_light', 'white')
+    data_dark=data.get('data_dark', 'white')
+    quiet_zone = data.get('quiet_zone', 'white')  # Access the 'text' value, default to '' if not found
     data_dark=data.get('dark_color', 'black')
-    data_light=data.get('light_color', 'white')
-
-
 
     qrcode = segno.make(qr_content)
+
 
     # Save the QR code to a file
     filename = f"{str(time.time())}.png"
@@ -39,12 +34,12 @@ def create_qr_code():
 # )
     qrcode.save(
         f"./static/qr-images/{filename}", 
-        scale=2, 
-        quiet_zone=border_color, 
+        scale=scaleValue, 
         light=light_main,
         dark=dark_main,
+        data_light=data_light,
         data_dark=data_dark,
-        data_light=data_light
+        quiet_zone=quiet_zone, 
     )
     
     # For this example, we'll just return it back in the response
@@ -53,3 +48,17 @@ def create_qr_code():
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
+
+
+'''
+qrcode.save(
+    "green_datamodules_qrcode.png",
+    scale=5,
+    light="lightblue",
+    dark="darkblue",
+    data_dark="green",
+    data_light="lightgreen",
+    quiet_zone="maroon",
+
+)
+'''
